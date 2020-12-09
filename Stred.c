61b2cc0401df855806c5ca27d78b1cc27984774b
JohnRussell13 <aleksandar.g1999@gmail.com>
2020-12-10 00:31:32 +0100

    Write string almost done

--- Stred.c
+++ Stred.c
@@ -19,9 +19,11 @@ static struct class *my_class;
 static struct device *my_device;
 static struct cdev *my_cdev;
 
-int storage[STORAGE_SIZE];
+char storage[STORAGE_SIZE];
 int pos = 0;
 int endRead = 0;
+char mods[6][9];
+unsigned char mods_len[6];
 
 int stred_open(struct inode *pinode, struct file *pfile);
 int stred_close(struct inode *pinode, struct file *pfile);
@@ -40,14 +42,14 @@ struct file_operations my_fops =
 
 int stred_open(struct inode *pinode, struct file *pfile) 
 {
-		printk(KERN_INFO "Succesfully opened file\n");
-		return 0;
+	printk(KERN_INFO "Succesfully opened file\n");
+	return 0;
 }
 
 int stred_close(struct inode *pinode, struct file *pfile) 
 {
-		printk(KERN_INFO "Succesfully closed file\n");
-		return 0;
+	printk(KERN_INFO "Succesfully closed file\n");
+	return 0;
 }
 
 ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) 
@@ -61,9 +63,9 @@ ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_
 		printk(KERN_INFO "Succesfully read from file\n");
 		return 0;
 	}
-	len = scnprintf(buff,BUFF_SIZE , "%d ", storage[pos]);
+	len = scnprintf(buff,BUFF_SIZE , "%c ", storage[pos]);
 	ret = copy_to_user(buffer, buff, len);
-	if(ret)
+	if (ret)
 		return -EFAULT;
 	pos ++;
 	if (pos == STORAGE_SIZE) {
@@ -75,96 +77,104 @@ ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_
 ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
 {
 	char buff[BUFF_SIZE];
-	int position, value;
+	char str_in[STORAGE_SIZE];
 	int ret;
 
 	ret = copy_from_user(buff, buffer, length);
-	if(ret)
+	if (ret)
 		return -EFAULT;
 	buff[length-1] = '\0';
 
-	ret = sscanf(buff,"%d,%d",&value,&position);
+	ret = sscanf(buff,"%s", str_in);
 
-	if(ret==2)//two parameters parsed in sscanf
+	if (ret!=1)//one parameters parsed in sscanf
 	{
-		if(position >=0 && position < STORAGE_SIZE)
-		{
-			storage[position] = value; 
-			printk(KERN_INFO "Succesfully wrote value %d in position %d\n", value, position); 
-		}
-		else
-		{
-			printk(KERN_WARNING "Position should be between 0 and 99\n"); 
-		}
-	}
-	else
-	{
-		printk(KERN_WARNING "Wrong command format\nexpected: n,m\n\tn-position\n\tm-value\n");
+		printk(KERN_WARNING "Wrong command format\nexpected: string=S\n\tS = input string\n");
+		return length;//maybe -1
 	}
 
+	//there are only few possibilities, so this won't be hard
+	
+	if (!strncmp(str_in,mods[0],mods_len[0])){
+		strcpy(storage,str_in);
+		printk(KERN_INFO "String stored correctly.");
+	}
 	return length;
 }
 
 static int __init stred_init(void)
 {
-   int ret = 0;
-	int i=0;
+	int ret = 0;
+   	int i = 0;
 
-	//Initialize array
-	for (i=0; i<STORAGE_SIZE; i++)
+	//init storage
+	for (i = 0; i < STORAGE_SIZE; i++)
 		storage[i] = 0;
+	//init mods
+	strcpy(mods[0], "string=");
+	strcpy(mods[1], "clear");
+	strcpy(mods[2], "shrink");
+	strcpy(mods[3], "append=");
+	strcpy(mods[4], "truncate=");
+	strcpy(mods[5], "remove=");
+	//init mods_len (faster than to do strlen every single time)
+	mods_len[0] = 7;
+	mods_len[1] = 5;
+	mods_len[2] = 6;
+   	mods_len[3] = 7; 
+	mods_len[4] = 9; 	
+	mods_len[5] = 7;
+		
+	ret = alloc_chrdev_region(&my_dev_id, 0, 1, "stred");
+   	if (ret){
+   		printk(KERN_ERR "failed to register char device\n");
+      		return ret;
+   	}
+   	printk(KERN_INFO "char device region allocated\n");
 
-   ret = alloc_chrdev_region(&my_dev_id, 0, 1, "stred");
-   if (ret){
-      printk(KERN_ERR "failed to register char device\n");
-      return ret;
-   }
-   printk(KERN_INFO "char device region allocated\n");
-
-   my_class = class_create(THIS_MODULE, "stred_class");
-   if (my_class == NULL){
-      printk(KERN_ERR "failed to create class\n");
-      goto fail_0;
-   }
-   printk(KERN_INFO "class created\n");
+   	my_class = class_create(THIS_MODULE, "stred_class");
+   	if (my_class == NULL){
+      		printk(KERN_ERR "failed to create class\n");
+      		goto fail_0;
+   	}
+   	printk(KERN_INFO "class created\n");
    
-   my_device = device_create(my_class, NULL, my_dev_id, NULL, "stred");
-   if (my_device == NULL){
-      printk(KERN_ERR "failed to create device\n");
-      goto fail_1;
-   }
-   printk(KERN_INFO "device created\n");
+   	my_device = device_create(my_class, NULL, my_dev_id, NULL, "stred");
+   	if (my_device == NULL){
+      		printk(KERN_ERR "failed to create device\n");
+      		goto fail_1;
+   	}
+   	printk(KERN_INFO "device created\n");
 
 	my_cdev = cdev_alloc();	
 	my_cdev->ops = &my_fops;
 	my_cdev->owner = THIS_MODULE;
 	ret = cdev_add(my_cdev, my_dev_id, 1);
-	if (ret)
-	{
-      printk(KERN_ERR "failed to add cdev\n");
+	if (ret){
+      		printk(KERN_ERR "failed to add cdev\n");
 		goto fail_2;
 	}
-   printk(KERN_INFO "cdev added\n");
-   printk(KERN_INFO "Hello world\n");
+   	printk(KERN_INFO "cdev added\n");
+   	printk(KERN_INFO "Hello world\n");
 
-   return 0;
+   	return 0;
 
-   fail_2:
-      device_destroy(my_class, my_dev_id);
-   fail_1:
-      class_destroy(my_class);
-   fail_0:
-      unregister_chrdev_region(my_dev_id, 1);
-   return -1;
+   	fail_2:
+      		device_destroy(my_class, my_dev_id);
+   	fail_1:
+      		class_destroy(my_class);
+   	fail_0:
+      		unregister_chrdev_region(my_dev_id, 1);
+   	return -1;
 }
 
 static void __exit stred_exit(void)
 {
-   cdev_del(my_cdev);
-   device_destroy(my_class, my_dev_id);
-   class_destroy(my_class);
-   unregister_chrdev_region(my_dev_id,1);
-   printk(KERN_INFO "Goodbye, cruel world\n");
+   	cdev_del(my_cdev);
+   	device_destroy(my_class, my_dev_id);
+   	class_destroy(my_class);
+    	unregister_chrdev_region(my_dev_id,1);
+   	printk(KERN_INFO "Goodbye, cruel world\n");
 }
 
 
